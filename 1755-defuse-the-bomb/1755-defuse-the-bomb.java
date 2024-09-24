e181fef9d3839afd7ee5f2915ced0c2cb432f624
SAKTHIVAISHNAVI N <125540529+sakthivaishnavi@users.noreply.github.com>
2024-09-24 14:50:11 +0530

    Time: 2 ms (57.93%), Space: 42 MB (88.26%) - LeetHub

--- 1755-defuse-the-bomb/1755-defuse-the-bomb.java
+++ 1755-defuse-the-bomb/1755-defuse-the-bomb.java
@@ -1,42 +1,29 @@
 class Solution {
-    public int[] decrypt(int[] code, int k) {
-        int n =code.length;
-        int sum=0;
-        int arr[] = new int[n];
-            if(k==0)
-            {
-                return new int[n];
-            }
-            if(k>0)
-            {
-                for(int i=1;i<=k;i++)
-                {
-                    sum += code[i%n];
-                }
-                for(int i=0;i<n;i++)
-                {
-                    arr[i]=sum;
-                    sum -= code[(i+1)%n];
-                    sum += code[(i+k+1) % n];
-                }
-            }
-            else if(k<0)
-            {
-                for(int i=k;i<0;i++)
-                {
-                    sum+=code[(n+i)%n];
-                }
-                for(int i=0;i<n;i++)
-                {
-                    arr[i]=sum;
-                    sum-= code[(n+i+k)%n];
-                    sum+=code[i%n];
-                }
-                
-            }
-        
+     public int[] decrypt(int[] code, int k) {
+        int[] ans = new int[code.length];
+        if (k == 0) {
+            return ans;
+        }
 
-        return arr;
-        
+        if (k > 0) {
+            for (int i = 0; i < code.length; i++) {
+                int sum = 0;
+                for (int j = 1; j <= k; j++) {
+                    sum += code[(i + j) % code.length];
+                }
+                ans[i] = sum;
+            }
+        }
+        else {
+            for (int i = 0; i < code.length; i++) {
+                int sum = 0;
+                for (int j = 1; j <= -1 * k; j++) {
+                    sum += code[(i - j + code.length) % code.length];
+
+                }
+                ans[i] = sum;
+            }
+        }
+        return ans;
     }
 }
