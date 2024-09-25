1b7b71ff3cc5aeefb295de767edd06c0235f117e
SAKTHIVAISHNAVI N <125540529+sakthivaishnavi@users.noreply.github.com>
2024-09-25 14:45:45 +0530

    Time: 1 ms (81.16%), Space: 41.6 MB (19.84%) - LeetHub

---
+++ 0290-word-pattern/0290-word-pattern.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+        String str[] = s.split(" ");
+        int indexP[] = new int[200];
+        HashMap<String, Integer> indexS = new HashMap<>();
+
+        if(pattern.length()!=str.length)
+        return false;
+
+        for(int i=0;i<pattern.length();i++)
+        {
+            String wrd= str[i];
+
+            if(indexP[pattern.charAt(i)]!= indexS.getOrDefault(wrd,0))
+            return false;
+
+            indexP[pattern.charAt(i)]=i+1;
+            indexS.put(wrd,i+1);
+        }
+        return true;        
+    }
+}
