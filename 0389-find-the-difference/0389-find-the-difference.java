50cafc37a89a3c90e9b4683e64908ce4567740c0
SAKTHIVAISHNAVI N <125540529+sakthivaishnavi@users.noreply.github.com>
2024-09-26 12:10:26 +0530

    Time: 1 ms (100%), Space: 41.4 MB (88.22%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.java
@@ -0,0 +1,14 @@
+class Solution {
+    public char findTheDifference(String s, String t) {
+        char ch =0;
+        for(char c : s.toCharArray())
+        {
+            ch^=c;
+        }
+         for(char c : t.toCharArray())
+        {
+            ch^=c;
+        }
+        return ch;
+    }
+}
