92aa021f85465abd8bcc23b036348c95fd256313
SAKTHIVAISHNAVI N <125540529+sakthivaishnavi@users.noreply.github.com>
2024-09-27 14:16:15 +0530

    Time: 1 ms (100%), Space: 44.2 MB (45.14%) - LeetHub

--- 2148-minimum-number-of-moves-to-seat-everyone/2148-minimum-number-of-moves-to-seat-everyone.java
+++ 2148-minimum-number-of-moves-to-seat-everyone/2148-minimum-number-of-moves-to-seat-everyone.java
@@ -1,13 +1,41 @@
 class Solution {
     public int minMovesToSeat(int[] seats, int[] students) {
-        Arrays.sort(seats);
-        Arrays.sort(students);
-        int sum=0;
-        for(int i=0;i<seats.length;i++)
-        {
-            sum+=Math.abs(seats[i]-students[i]);
+        int max1=0,max2=0;
+        for(int i=0;i<seats.length;i++){
+         max1= Math.max(max1,seats[i]);
+         max2= Math.max(max2,students[i]);
         }
-        return sum;
-        
+
+        int count1[] = new int[max1+1];
+        int count2[] = new int[max2+1];
+
+        for(int i=0;i<seats.length;i++){
+            count1[seats[i]]++;
+            count2[students[i]]++;
+        }
+        int n=0;
+        for(int i=0;i<count1.length;i++){
+            while(count1[i]>0){
+                seats[n]=i;
+                n++;
+                count1[i]--;
+            }
+        }
+        n=0;
+        for(int i=0;i<count2.length;i++)
+        {
+            while(count2[i]>0){
+                students[n]=i;
+                n++;
+                count2[i]--;
+            }
+        }
+        n=0;
+        int sum=0;
+        while(n<seats.length){
+            sum+=Math.abs(students[n]-seats[n]);
+            n++;
+        }
+        return sum;        
     }
 }
