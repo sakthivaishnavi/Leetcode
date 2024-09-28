fc7cc9ef14be3bae1427ddf8b70a59a0c5a41023
SAKTHIVAISHNAVI N <125540529+sakthivaishnavi@users.noreply.github.com>
2024-09-28 23:39:24 +0530

    Time: 4 ms (100%), Space: 44.7 MB (46.3%) - LeetHub

--- 0859-design-circular-deque/0859-design-circular-deque.java
+++ 0859-design-circular-deque/0859-design-circular-deque.java
@@ -1,56 +1,102 @@
-import java.util.concurrent.LinkedBlockingDeque;
-import java.util.Deque;
 class MyCircularDeque {
-        public Deque<Integer> d ;
-        public int capacity;
+    int[] v;
+    int front,back,size,capacity;
+
     public MyCircularDeque(int k) {
-        this.capacity =k;
-        this.d =  new LinkedBlockingDeque<>(k);
+        v = new int[k];
+        Arrays.fill(v,-1);
+        front=0;
+        back=0;
+        size=0;
+        capacity=k;        
     }
     
     public boolean insertFront(int value) {
-        if(d.size()<capacity){
-          d.addFirst(value);
-          return true;
-        }
-        return false;        
+        if(isFull())
+        return false;
+
+        if(front==0)
+        front=capacity-1;
+        else
+        front--;      
+
+        v[front]=value;
+        size++;
+        return true;  
     }
     
     public boolean insertLast(int value) {
-        if(d.size()<capacity){
-         d.addLast(value);  
-         return true;
-        }
-        return false;      
+        if(isFull())
+        return false;
+
+        v[back] = value;
+        if(back== capacity-1)
+        back=0;
+        else
+        back++;
+
+        size++;
+        return true;        
     }
     
     public boolean deleteFront() {
-        return d.pollFirst()!=null;        
+        if(isEmpty())
+        return false;
+
+        v[front]=-1;
+
+        if(front==capacity-1)
+        front=0;
+        else
+        front++;
+
+        size--;
+        return true;        
     }
     
     public boolean deleteLast() {
-        return d.pollLast()!=null;        
+        if(isEmpty())
+        return false;
+
+         if(back==0)
+         back=capacity-1;
+         else
+         back--;
+         v[back]=-1;
+
+         size--;
+         return true;        
     }
     
     public int getFront() {
-       Integer val = d.peekFirst();
-       return val == null ? -1 :val;
-        
+        if(isEmpty())
+        return -1;
+
+        return v[front];        
     }
     
     public int getRear() {
-        Integer val = d.peekLast();
-       return val == null ? -1 :val;
-        
+        if(isEmpty())
+        return -1;
+
+        if(back==0)
+        return v[capacity-1];
+        else
+        return v[back-1];      
     }
     
     public boolean isEmpty() {
-        return d.isEmpty();               
+        if(size==0)
+        return true;
+        else
+        return false;        
     }
     
     public boolean isFull() {
-        return d.size()==capacity;
-                
+        if(size==capacity)
+        return true;
+        else
+        return false;        
     }
 }
 
